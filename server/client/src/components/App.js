56f3c581ca67acc3abe1a615f71d13d0ecc49dd3
cyberlearner13 <arkadeepmukherjee2012@gmail.com>
2020-06-14 11:33:56 +0530

    added a header component in UI and backend init

--- server/client/src/components/App.js
+++ server/client/src/components/App.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Header from './Header';
 import Login from './Login';
 import Logout from './Logout';
 import Home from './Home';
 import UserDataForm from './UserDataForm';
 
 const App = () => {
-    return (
-        <div>
-            <h1>Hooked In React</h1>
-            <Router>
-                <Switch>
-                    <Route path="/protectedInfo" component={UserDataForm} />
-                    <Route path="/logout" component={Logout} />
-                    <Route path="/login" component={Login} />
-                    <Route exact path="/" component={Home} />
-                </Switch>
-            </Router>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header />
+      <Router>
+        <Switch>
+          <Route path='/protectedInfo' component={UserDataForm} />
+          <Route path='/logout' component={Logout} />
+          <Route path='/login' component={Login} />
+          <Route exact path='/' component={Home} />
+        </Switch>
+      </Router>
+    </div>
+  );
+};
 export default App;
---
+++ server/client/src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => (
+  <div className='header'>
+    <h2>Hooked in React</h2>
+  </div>
+);
+
+export default Header;
